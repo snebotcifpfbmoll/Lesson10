5321956048f3d5e4bbfd3cf2d677be591bf53af4
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2021-01-31 12:29:07 +0100

    finished animations

--- app/src/main/java/com/serafinebot/dint/lesson10/SplashActivity.java
+++ app/src/main/java/com/serafinebot/dint/lesson10/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.serafinebot.dint.lesson10;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -15,12 +16,28 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
         TextView logo1 = findViewById(R.id.TextViewTopTitle);
         Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         logo1.startAnimation(fade1);
 
         TextView logo2 = findViewById(R.id.TextViewBottomTitle);
         Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
         logo2.startAnimation(fade2);
 
         Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
